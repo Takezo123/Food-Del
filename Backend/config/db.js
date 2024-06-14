7d57d279518d06bc67bdd4a525f4d2c5bde0e3bc
Takezo123 <daniilbulatkin@gmail.com>
2024-06-14 04:52:34 +0300

    aboba

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://greatstack:186312@cluster0.ovanjzw.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://admin:admin@cluster0.9ww5wjm.mongodb.net/retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log("DB Connected"));
 }
